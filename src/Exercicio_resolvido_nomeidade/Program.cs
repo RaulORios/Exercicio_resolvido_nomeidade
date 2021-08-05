bf0de9e6bb35c6624000145d1b15599ddfe401ea
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:00:41 -0300

    Program.cs adicionado

---
+++ src/Exercicio_resolvido_nomeidade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_resolvido_nomeidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet1 = Console.ReadLine().Split(' ');
+            string nome1 = vet1[0];
+            int idade1 = int.Parse(vet1[1]);
+
+            string[] vet2 = Console.ReadLine().Split(' ');
+            string nome2 = vet2[0];
+            int idade2 = int.Parse(vet2[1]);
+
+            double media = (double)(idade1 + idade2) / 2.0;
+
+            Console.WriteLine($"A idade média de {nome1} e {nome2} é de {media.ToString("F1",CultureInfo.InvariantCulture)} anos");
+
+            Console.ReadKey();
+                       
+        }
+    }
+}
